86a2bb667551158faaf2b7714cc9342d523c612d
Шебаниц Виталий <shebanic95@gmail.com>
2020-09-09 17:06:28 +0300

    scaled architecture: first step

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CoreModule } from '@core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { LayoutModule } from '@layout/layout.module';
+import { ViewsModule } from '@views/views.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CoreModule,
+    SharedModule,
+    LayoutModule,
+    ViewsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/views/views.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ViewsRoutingModule } from './views-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+
+
+@NgModule({
+  declarations: [DashboardComponent, NotFoundComponent],
+  imports: [
+    CommonModule,
+    ViewsRoutingModule
+  ]
+})
+export class ViewsModule { }
