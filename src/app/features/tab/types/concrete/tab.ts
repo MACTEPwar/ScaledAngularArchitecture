75dc4b867c05c07f96944b063b72537ccc30839f
Шебаниц Виталий <shebanic95@gmail.com>
2020-09-29 09:47:49 +0300

    коменты

--- src/app/features/tab/types/concrete/tab.ts
+++ src/app/features/tab/types/concrete/tab.ts
@@ -1,10 +1,16 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 import { ITab } from '../interface/i-tab';
 
-export class Tab implements ITab{
+/** Класс для табов */
+export class Tab implements ITab {
+  /** Отображаемое название */
   name: string;
+  /** Картика (FontAwesome) */
   image?: IconDefinition;
+  /** Ссылка, по которой будет переходить при нажатии на таб */
   url: string;
+  /** Можно ли закрыть таб? */
   canClose: boolean;
+  /** Активный? */
   active: boolean;
 }
--- src/app/features/tab/types/interface/i-tab.ts
+++ src/app/features/tab/types/interface/i-tab.ts
@@ -1,9 +1,15 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
 
+/** Интерфейс для табов */
 export interface ITab {
+    /** Отображаемое название */
     name: string;
+    /** Картика (FontAwesome) */
     image?: IconDefinition;
+    /** Ссылка, по которой будет переходить при нажатии на таб */
     url: string;
+    /** Можно ли закрыть таб? */
     canClose: boolean;
+    /** Активный? */
     active: boolean;
 }
